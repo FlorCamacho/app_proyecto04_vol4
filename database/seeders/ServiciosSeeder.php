e80efe60a1196ac96fdc451fccde43bbc33e6c0c
Fernanda Camacho <8122110005@utchetumal.edu.mx>
2024-03-03 14:45:09 -0500

    MongoDB Models Seeder

--- database/seeders/ServiciosSeeder.php
+++ database/seeders/ServiciosSeeder.php
@@ -8,9 +8,6 @@ use App\Models\Servicios;
 
 class ServiciosSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
         {
             Servicios::create([
